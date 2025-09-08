64ff9015d66890d12d50ba6dffe78862f492c1cd
ai-eval <ai.eval@proton.me>
2025-09-07 21:05:55 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	  site: ' https://ai-eval.github.io/test-starlight',
 	  outDir: './docs/',
 	  build: {
 	  assets: '_astro'
